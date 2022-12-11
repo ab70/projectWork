d57978b7a5eb3f03577faade962c2cbd6c33a679
ab70 <nurulabrar2369@gmail.com>
2022-12-11 23:47:08 +0600

    cors error check

--- backend/index.js
+++ backend/index.js
@@ -33,6 +33,7 @@ app.use(flash())
 app.use(function(req, res, next) {  
     res.header('Access-Control-Allow-Origin', req.headers.origin);
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header('Access-Control-Allow-Credentials', true);
     next();
 });  
 // session create in mongodb 
