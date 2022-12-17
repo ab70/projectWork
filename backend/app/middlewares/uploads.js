260ff6bde848095501c7a9a4c138d36dec200135
ab70 <nurulabrar2369@gmail.com>
2022-12-17 11:51:17 +0600

    multer category

--- backend/app/middlewares/uploads.js
+++ backend/app/middlewares/uploads.js
@@ -8,7 +8,7 @@ const storages = multer.diskStorage({
 
     },
     filename:(req,file,cb)=>{
-        cb(null,field.filename+'_'+Date.now()+'_'+file.originalname)
+        cb(null,file.fieldname+'_'+Date.now()+'_'+file.originalname)
     }
 })
 
