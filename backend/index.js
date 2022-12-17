f04fc987306981340c18a6637e013b6f53ee5cb1
ab70 <nurulabrar2369@gmail.com>
2022-12-17 16:17:15 +0600

    img

--- backend/index.js
+++ backend/index.js
@@ -61,7 +61,7 @@ app.use((req,res,next)=>{
 })
 
 
-app.use(express.static(path.join(__dirname,'backend/public')))
+app.use(express.static(path.join(__dirname,'public')))
 
 //required api.js file and passed app to there
 require('./routes/api')(app)
