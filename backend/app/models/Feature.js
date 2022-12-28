1fe837764c8197d8822cef07c849f520dae8d399
ab70 <nurulabrar2369@gmail.com>
2022-12-28 23:35:05 +0600

    ch

--- backend/app/models/Feature.js
+++ backend/app/models/Feature.js
@@ -2,9 +2,13 @@ const mongoose = require('mongoose')
 
 
 const FeatureSchema = new mongoose.Schema({
+    
     featureName: { type: String, requred: true, trim: true},
     featureType: {type: String, requred: true, trim: true },
+    featureOrdering:{type:Number, default:0},
+    status:{type:String, enum:['active','inactive'], default:'active'},
     options: [{optionName: {type: String, required: true, trim: true}}]
+
 },{timestamps: true})
 
 module.exports = mongoose.model("Feature",FeatureSchema)
