4c210ced5198a8d7f56ade7267521e4920f3c7df
ab70 <nurulabrar2369@gmail.com>
2022-12-31 16:23:43 +0600

    p

--- backend/app/models/Product.js
+++ backend/app/models/Product.js
@@ -16,7 +16,7 @@ const ProductSchema = new mongoose.Schema({
     showTill: {type: Date, default: moment().add(60, 'days').format('LL')},
     notificationDialogue: {type:String, trim: true, default:''},
     productImgs :[ {img: {type: String, trim:true}, approved:{type: Boolean, default: false}, longImg:{type: Boolean,  default: false }}],
-    imageChanged:{type:String, enum:['yes','no'], default:'yes'},
+    imageChanged:{type:Boolean, default:true},
     //packageId:{type: mongoose.Schema.Types.ObjectId, ref:'Package', default:''},
     boosted:{type:Boolean, default:false},
     publishedAt:{type:Date, default: moment().format('LL')},
